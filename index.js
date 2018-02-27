684f3acbc59b5e8357c094fa5173af0f811fd607
Matt Williams <matthew.williams@healthcatalyst.com>
2018-02-27 00:24:47 -0800

    markers and calls working, need to render results in html

--- index.js
+++ index.js
@@ -2,56 +2,78 @@
 
 
 let map;
+let markers = [];
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.732080, lng: -122.553614},
     zoom: 12
   });
-const loc_port = {lat: 48.693351, lng: -122.610278};  
-const mark_port = new google.maps.Marker({
-  position: loc_port,
-  map:map,
-  customInfo:"Marker1",
-  title:"Portage Island"
-});
+// const loc_port = {lat: 48.693351, lng: -122.610278};  
+// const mark_port = new google.maps.Marker({
+//   position: loc_port,
+//   map:map,
+//   customInfo:"Marker1",
+//   title:"Portage Island",
+//   clickable:true,
+// });
 
-const loc_bellBay = {lat: 48.752142, lng: -122.504546};
-const mark_bellBay = new google.maps.Marker({
-    position: loc_bellBay,
-    map:map,
-    customInfo:"Marker2",
-    title:"Squalicum Harbor"
-});
+// const loc_bellBay = {lat: 48.752142, lng: -122.504546};
+// const mark_bellBay = new google.maps.Marker({
+//     position: loc_bellBay,
+//     map:map,
+//     customInfo:"Marker2",
+//     title:"Squalicum Harbor",
+//     clickable: true,
+// });
 
 google.maps.event.addListener(map, 'click', function(event) {
-        addMarker(event.latLng, map);
-        const markerTitle = toString(event.latLng);
-        getDataFromApi(event.latLng);
-  });
-
+    clearMarkers();
+    addMarker(event.latLng, map);
+});
 }
 
-function addMarker(location, map) {
-    var marker = new google.maps.Marker({
+ function addMarker(location, map) {
+    const marker = new google.maps.Marker({
         position:location,
         map:map,
-    })
+    });
+    markers.push(marker);
+    const lat = marker.getPosition().lat();
+    const lng = marker.getPosition().lng();
+    const latLong = lat+','+lng
+    getDataFromApi(latLong);
+    console.log(latLong);
 }
 
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
 
-function getDataFromApi(latLng, callback) {  
-    // $.ajax({
-    //     url : "http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/conditions/q/48.693351,-122.610278.json",
-    //     dataType : "jsonp",
-    //     success : function(parsed_json) {
-    //         let location = parsed_json['location']['city'];
-    //         let temp_f = parsed_json['current_observation']['temp_f'];
-    //         let wind_spd = parsed_json['current_observation']['wind_mph'];
-    //         let wind_dir = parsed_json['current_observation']['wind_dir'];
-    //         console.log("Current temperature in " + location + " is: " + temp_f+" and the wind is blowing from the "+wind_dir+" at "+wind_spd+" mph");
-    //         console.log(parsed_json);
-    //     }
-    //     });
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
+    setMapOnAll(null);
+    markers = [];
+  }
+
+function getDataFromApi(latLong) {  
+    const conditionsUrl =`http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/conditions/q/${latLong}.json`;
+    $.ajax({
+        url: conditionsUrl,
+        //url : "http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/conditions/q/48.693351,-122.610278.json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            console.log(parsed_json);
+            let location = parsed_json['location']['city'];
+            let temp_f = parsed_json['current_observation']['temp_f'];
+            let wind_spd = parsed_json['current_observation']['wind_mph'];
+            let wind_dir = parsed_json['current_observation']['wind_dir'];
+            console.log("Current temperature in " + location + " is: " + temp_f+" and the wind is blowing from the "+wind_dir+" at "+wind_spd+" mph");
+            //console.log(parsed_json);
+        }
+        });
     $.ajax({
         url:"http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/tide/q/48.693351,-122.610278.json",
         dataType: "jsonp",
@@ -59,31 +81,19 @@ function getDataFromApi(latLng, callback) {
             console.log(parsed_json);
             let results = [];
             for(var i = 0; i < parsed_json.tide.tideSummary.length; i++) {
-                if ((parsed_json.tide.tideSummary[i].data.type === "Low Tide") || (parsed_json.tide.tideSummary[i].data.type === "High Tide")
-                   || (parsed_json.tide.tideSummary[i].data.type = "Sunrise") || (parsed_json.tide.tideSummary[i].data.type = "Sunset")) {
-                    results.push(parsed_json.tide.tideSummary[i]);
-                    
+                if ((parsed_json.tide.tideSummary[i].data.type === "Low Tide") || (parsed_json.tide.tideSummary[i].data.type === "High Tide")) {
+                    results.push(parsed_json.tide.tideSummary[i]);   
                 }
-            
             }
-            return(results);
             console.log(results);
-            //let tideLocation = parsed_json['location']['city'];
-            // let tideSummary = parsed_json.tideSummary.map((data, index) =>
-            //     renderResult(data));
-            // console.log(tideSummary);
-            //let tideHeight = parsed_json['tideInfo']['tideSite'];
-            // // let tideType = parsed_json['tideSummary']['type'];
-            // // let tideTime = parsed_json['tideSummary']['pretty'];
-            // // console.log(tideType+": "+tideHeight+" ( "+tideTime+" )")
-            // console.log(tideLocation);
-            // console.log(parsed_json);
+            console.log(results[0].data.type+" will be "+results[0].data.height+" at "+results[0].date.pretty);
         }
     })
 }
 
-function renderResult(result) {
-    console.log(result);
+
+function displayResults() {
+    $('.js-tides').html(result);
 }
 
 function listenSubmit() {
