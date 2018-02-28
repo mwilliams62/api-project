56b8ed7c3a5f3b417844e59fe985efa00e409f2e
Matt Williams <matthew.williams@healthcatalyst.com>
2018-02-28 12:06:03 -0800

    commit before attempting big changes

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function setMapOnAll(map) {
     }
   }
 
-  // Removes the markers from the map, but keeps them in the array.
+  // Removes the markers from the map and empties array
   function clearMarkers() {
     setMapOnAll(null);
     markers = [];
@@ -62,14 +62,16 @@ function getDataFromApi(latLong) {
     const conditionsUrl =`http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/conditions/q/${latLong}.json`;
     $.ajax({
         url: conditionsUrl,
-        //url : "http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/conditions/q/48.693351,-122.610278.json",
         dataType : "jsonp",
-        success : function(parsed_json) {
-            console.log(parsed_json);
-            let location = parsed_json['location']['city'];
-            let temp_f = parsed_json['current_observation']['temp_f'];
-            let wind_spd = parsed_json['current_observation']['wind_mph'];
-            let wind_dir = parsed_json['current_observation']['wind_dir'];
+        success : function(parsed_obs_json) {
+            let obs = [];
+            console.log(parsed_obs_json);
+            obs.push(parsed_obs_json.current_observation);
+            console.log(obs);
+            let location = parsed_obs_json['location']['city'];
+            let temp_f = parsed_obs_json['current_observation']['temp_f'];
+            let wind_spd = parsed_obs_json['current_observation']['wind_mph'];
+            let wind_dir = parsed_obs_json['current_observation']['wind_dir'];
             console.log("Current temperature in " + location + " is: " + temp_f+" and the wind is blowing from the "+wind_dir+" at "+wind_spd+" mph");
             //console.log(parsed_json);
         }
@@ -77,23 +79,30 @@ function getDataFromApi(latLong) {
     $.ajax({
         url:"http://api.wunderground.com/api/ddcfcefb488ad1af/geolookup/tide/q/48.693351,-122.610278.json",
         dataType: "jsonp",
-        success :  function(parsed_json) {
-            console.log(parsed_json);
-            let results = [];
-            for(var i = 0; i < parsed_json.tide.tideSummary.length; i++) {
-                if ((parsed_json.tide.tideSummary[i].data.type === "Low Tide") || (parsed_json.tide.tideSummary[i].data.type === "High Tide")) {
-                    results.push(parsed_json.tide.tideSummary[i]);   
+        success :  function(parsed_tide_json) {
+            console.log(parsed_tide_json);
+            let tides = [];
+            for(var i = 0; i < parsed_tide_json.tide.tideSummary.length; i++) {
+                if ((parsed_tide_json.tide.tideSummary[i].data.type === "Low Tide") || (parsed_tide_json.tide.tideSummary[i].data.type === "High Tide")) {
+                    tides.push(parsed_tide_json.tide.tideSummary[i]);   
                 }
             }
-            console.log(results);
-            console.log(results[0].data.type+" will be "+results[0].data.height+" at "+results[0].date.pretty);
+            displayTideResults(tides);
+            console.log(tides);
+            console.log(tides[0].data.type+" will be "+tides[0].data.height+" at "+tides[0].date.pretty);
         }
     })
 }
 
+function renderResult(result) {
+    console.log(result);
+}
 
-function displayResults() {
-    $('.js-tides').html(result);
+function displayTideResults(info) {
+    console.log(info);
+    renderResult(info);
+    $('.js-tides').html(info);
+    
 }
 
 function listenSubmit() {
